ae1aad4c6d804053d4406105314dfd73f23ddeb5
laquiante <33266194+laquiante@users.noreply.github.com>
2022-01-31 08:54:44 +0100

    Update ztp.sh

--- ztp.sh
+++ ztp.sh
@@ -15,7 +15,7 @@ if [ -f "$FILE" ]; then
 fi
 
 chown -R cumulus:cumulus /var/lib/cumulus/ztp/test.txt
-echo "Cumulus Linux Boot Camp 2022"  | tee -a test.txt
+echo "Cumulus Linux Boot Camp 2022"  | tee -a /var/lib/cumulus/ztp/test.txt
 
 #CUMULUS-AUTOPROVISIONING
 
